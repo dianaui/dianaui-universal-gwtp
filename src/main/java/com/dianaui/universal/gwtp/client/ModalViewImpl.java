ff1b3b364f3b11c44b39171ac12ba0bb294926c1
Alexey Zhokhov <donbeave@gmail.com>
2015-01-29 12:45:10 +0800

    Upgrade to GWTP 1.4.

--- src/main/java/com/dianaui/universal/gwtp/client/ModalViewImpl.java
+++ src/main/java/com/dianaui/universal/gwtp/client/ModalViewImpl.java
@@ -22,6 +22,9 @@ package com.dianaui.universal.gwtp.client;
 import com.dianaui.universal.core.client.event.HideEvent;
 import com.dianaui.universal.core.client.event.HideHandler;
 import com.dianaui.universal.core.client.ui.Modal;
+import com.google.gwt.event.logical.shared.ResizeEvent;
+import com.google.gwt.event.logical.shared.ResizeHandler;
+import com.google.gwt.user.client.Window;
 import com.google.web.bindery.event.shared.EventBus;
 import com.google.web.bindery.event.shared.HandlerRegistration;
 import com.gwtplatform.mvp.client.PopupView;
@@ -30,16 +33,21 @@ import com.gwtplatform.mvp.client.PopupViewImpl;
 import com.gwtplatform.mvp.client.ViewImpl;
 import com.gwtplatform.mvp.client.proxy.NavigationEvent;
 import com.gwtplatform.mvp.client.proxy.NavigationHandler;
+import com.gwtplatform.mvp.client.view.CenterPopupPositioner;
+import com.gwtplatform.mvp.client.view.PopupPositioner;
 
 /**
  * @author <a href='mailto:donbeave@gmail.com'>Alexey Zhokhov</a>
  */
 public abstract class ModalViewImpl extends ViewImpl implements PopupView {
 
-    private final EventBus eventBus;
-    private PopupViewCloseHandler popupViewCloseHandler;
     private HandlerRegistration autoHideHandler;
+
     private HandlerRegistration closeHandlerRegistration;
+    private PopupViewCloseHandler popupViewCloseHandler;
+    private final EventBus eventBus;
+
+    private PopupPositioner positioner;
 
     /**
      * The {@link PopupViewImpl} class uses the {@link EventBus} to listen to
@@ -50,7 +58,29 @@ public abstract class ModalViewImpl extends ViewImpl implements PopupView {
      * @param eventBus The {@link EventBus}.
      */
     protected ModalViewImpl(final EventBus eventBus) {
+        this(eventBus, new CenterPopupPositioner());
+    }
+
+    /**
+     * @param eventBus   The {@link EventBus}.
+     * @param positioner The {@link PopupPositioner} used to position the popup onReveal();
+     * @see com.gwtplatform.mvp.client.view.CenterPopupPositioner
+     * @see com.gwtplatform.mvp.client.view.RelativeToWidgetPopupPositioner
+     * @see com.gwtplatform.mvp.client.view.TopLeftPopupPositioner
+     */
+    protected ModalViewImpl(EventBus eventBus, PopupPositioner positioner) {
         this.eventBus = eventBus;
+        setPopupPositioner(positioner);
+        if (repositionOnWindowResize()) {
+            Window.addResizeHandler(new ResizeHandler() {
+                @Override
+                public void onResize(ResizeEvent event) {
+                    if (isShowing()) {
+                        showAndReposition();
+                    }
+                }
+            });
+        }
     }
 
     public boolean isShowing() {
@@ -59,39 +89,35 @@ public abstract class ModalViewImpl extends ViewImpl implements PopupView {
 
     @Override
     public void center() {
-        asModal().show();
+        // no op this method is here for legacy compatibility.
+        // since by default the popup positioner centers the popup only
+        // popups using a non default positioner would be affected by this method.
+        // And if you're using popup positioners you won't be calling this method.
     }
 
     @Override
     public void hide() {
         asModal().hide();
 
-        popupViewCloseHandler = null;
-
-        // remove close handler
-        if (closeHandlerRegistration != null) {
-            closeHandlerRegistration.removeHandler();
-            closeHandlerRegistration = null;
+        // events will not fire call closeHandler manually.
+        if (popupViewCloseHandler != null) {
+            popupViewCloseHandler.onClose();
         }
     }
 
     @Override
-    public void setAutoHideOnNavigationEventEnabled(final boolean autoHide) {
+    public void setAutoHideOnNavigationEventEnabled(boolean autoHide) {
         if (autoHide) {
             if (autoHideHandler != null) {
                 return;
             }
-
-            autoHideHandler = eventBus.addHandler(NavigationEvent.getType(), new NavigationHandler() {
-                @Override
-                public void onNavigation(NavigationEvent navigationEvent) {
-                    if (popupViewCloseHandler != null) {
-                        popupViewCloseHandler.onClose();
-                    } else {
-                        hide();
-                    }
-                }
-            });
+            autoHideHandler = eventBus.addHandler(NavigationEvent.getType(),
+                    new NavigationHandler() {
+                        @Override
+                        public void onNavigation(NavigationEvent navigationEvent) {
+                            hide();
+                        }
+                    });
         } else {
             if (autoHideHandler != null) {
                 autoHideHandler.removeHandler();
@@ -102,22 +128,27 @@ public abstract class ModalViewImpl extends ViewImpl implements PopupView {
     @Override
     public void setCloseHandler(final PopupViewCloseHandler popupViewCloseHandler) {
         this.popupViewCloseHandler = popupViewCloseHandler;
-
-        if (closeHandlerRegistration != null) {
-            closeHandlerRegistration.removeHandler();
-        }
-        if (popupViewCloseHandler == null) {
-            closeHandlerRegistration = null;
-        } else {
-            closeHandlerRegistration = asModal().addHandler(new HideHandler() {
+        if (closeHandlerRegistration == null) {
+            closeHandlerRegistration = asModal().addHideHandler(new HideHandler() {
                 @Override
                 public void onHide(HideEvent hideEvent) {
-                    popupViewCloseHandler.onClose();
+                    if (ModalViewImpl.this.popupViewCloseHandler != null) {
+                        ModalViewImpl.this.popupViewCloseHandler.onClose();
+                    }
                 }
-            }, HideEvent.getType());
+            });
         }
     }
 
+    @Override
+    public void setPopupPositioner(PopupPositioner popupPositioner) {
+        this.positioner = popupPositioner;
+    }
+
+    @Override
+    public void setPosition(int left, int top) {
+        // TODO
+    }
 
     @Override
     public void show() {
@@ -125,14 +156,32 @@ public abstract class ModalViewImpl extends ViewImpl implements PopupView {
     }
 
     @Override
-    @Deprecated
-    public void setPosition(final int left, final int top) {
-        // not implemented
+    public void showAndReposition() {
+        onReposition();
+
+        // TODO
+        asModal().show();
     }
 
-    protected EventBus getEventBus() {
-        return eventBus;
+    /**
+     * Override this method to add custom logic that runs before the popup is repositioned.
+     * By default the popup will be repositioned on resize and this method will be called.
+     * So you can add any resize logic here as well.
+     */
+    protected void onReposition() {
     }
+
+    /**
+     * By default PopupViews will reposition themselves when the window is resized.
+     * If you don't want the popup to be repositioned or want to handle the resize yourself
+     * overide this method to return false.
+     *
+     * @return whether to reposition on resize.
+     */
+    protected boolean repositionOnWindowResize() {
+        return true;
+    }
+
 
     /**
      * Retrieves this view as a {@link Modal}. See {@link #asWidget()}.
@@ -141,6 +190,10 @@ public abstract class ModalViewImpl extends ViewImpl implements PopupView {
      */
     protected Modal asModal() {
         return (Modal) asWidget();
+    }
+
+    protected EventBus getEventBus() {
+        return eventBus;
     }
 
 }
